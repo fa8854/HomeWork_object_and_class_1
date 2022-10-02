bf2836dde18a371c0aa36ba44a335fee7a5fbe4e
fa8854 <111686993+fa8854@users.noreply.github.com>
2022-10-02 17:00:31 +0400

    Домашняя работа по объектам и классам

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Алекс", "Кош");
+        Author author2 = new Author("Лев", "Толстой");
+        Book voinaMir = new Book("Война и мир", author2, 1911);
+        Book greg = new Book("Глаз", author1, 2012);
+        System.out.println(voinaMir.bookInfo());
+        System.out.println(greg.bookInfo());
 
+        voinaMir.setPublicationYear(2011);
+        System.out.println(voinaMir.bookInfo());
+        greg.setPublicationYear(2022);
+        System.out.println(greg.bookInfo());
     }
 }
